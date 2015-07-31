fa9cda975e33d3f371e8293403ce9ffc6810340c
vigneshvar.a.s <viggates@gmail.com>
2015-07-31 20:58:42 +0000

    Updated bootstrap.sh tp support cassandra and generic role based bootstrap execution Added cassandra-bootstrap and clustring script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,12 @@
 CONSULURL=$1
 TOKEN=$2
 JSAWK_URL=$3
+TOTAL=$4
+REPOURL=$5
+NAME=$6
+MEM=$7
+HEAP=$8
+
 CONSUL_DIR=/opt/consul/
 RUNONCE=/usr/bin/runonce
 
@@ -57,6 +63,26 @@ then
 		#./consul agent -server -data-dir data/ -bind 15.1.0.8 -bootstrap-expect 1
                 echo "* * * * * $RUNONCE $CONSUL_DIR/consul agent -server -bootstrap-expect 1 -data-dir $CONSUL_DIR/data -config-file /etc/enc.json  -bind $IP >> /var/log/consul.log 2>&1 &" > /tmp/cmd
 		crontab < /tmp/cmd
+
+		join=1
+                count=1
+                sleep 10
+                while [ $join -eq 1 ]
+                do
+                        join=`$CONSUL_DIR/consul members | grep  "connection refused" | wc -l`
+                        count=`expr $count + 1`
+                        sleep 1
+                        if [ $count -eq 500 ]
+                        then
+                                echo "consul failed to start"
+                                exit
+                        fi
+                done
+		# Add data to consul from server
+		sleep 40
+		curl -X PUT -d "$TOTAL"  http://localhost:8500/v1/kv/cassandra/node_count
+		curl -X PUT -d "$REPOURL/cassandra-bootstrap.sh" http://localhost:8500/v1/kv/role/cassandra
+
 	else
                 echo "* * * * * $RUNONCE $CONSUL_DIR/consul agent -data-dir $CONSUL_DIR/data -config-file /etc/enc.json  -bind $IP >> /var/log/consul.log 2>&1 &" > /tmp/cmd
 		masterip=`curl http://169.254.169.254/openstack/2013-10-17/meta_data.json  | jsawk 'return this.meta.consul_master'`
@@ -69,12 +95,30 @@ then
 			join=`$CONSUL_DIR/consul join $masterip | grep  "connection refused" | wc -l`
 			count=`expr $count + 1`
 			sleep 1
-			if [ $count -eq 300 ]
+			if [ $count -eq 500 ]
 			then
 				echo "master join failed"
 				exit
 			fi
 		done
+		join=""
+                count=1
+                sleep 10
+                while [ "$join" == "" ]
+                do
+                        join=`curl http://localhost:8500/v1/kv/role/$ROLE?raw`
+                        count=`expr $count + 1`
+                        sleep 1
+                        if [ $count -eq 500 ]
+                        then
+                                echo "Did not get role bootstrap"
+                                exit
+                        fi
+                done
+		cd /opt/
+		curl $join -o role-bootstrap.sh
+		sh -x role-bootstrap.sh $NAME $REPOURL $MEM $HEAP
+
 	fi
 #	crontab < /tmp/cmd
 fi
---
+++ cassandra-bootstrap.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+# Download cassandra tar
+# Download cassandra yaml
+# Download cassandra sh
+# Download cassandra service
+
+# Check if its ip is added to nodes key in consul
+# If not added kindly add it
+
+# Check the total number of nodes from consul equal to number of nodes in key
+# if yes 
+#	configure cassandra yaml
+#	configure cassandra sh
+#	enable service
+# If not loop it back  and wait till times out
+# If times out update error key on consul as failed
+
+CLUSTERNAME=$1
+REPOURL=$2
+MEM=$3
+HEAP=$4
+
+
+curl $REPOURL/cassandra.tar -o /opt/cassandra.tar
+curl $REPOURL/cassandra.yaml -o /opt/cassandra.yaml
+curl $REPOURL/cassandra.sh -o /opt/cassandra.sh
+curl $REPOURL/cassandra.service -o /opt/cassandra.service
+curl $REPOURL/jdk-7u79-linux-x64 -o /opt/jdk-7u79-linux-x64
+
+cd /opt/
+tar xf cassandra.tar
+cp /opt/cassandra.yaml /opt/cassandra/conf/
+cp /opt/cassandra.sh /opt/cassandra/conf/
+cp /opt/cassandra.service /etc/systemd/system/
+
+status=`cat /etc/profile | grep JAVA_HOME | wc -l`
+
+if [ $status -eq 0 ]
+then
+	tar xf jdk-7u79-linux-x64
+	echo "export JAVA_HOME=/opt/jdk1.7.0_79/" >> /etc/profile
+	echo "export PATH=\"\$PATH:\$JAVA_HOME/bin/\"" >> /etc/profile
+	ln -s /opt/jdk1.7.0_79/bin/java /usr/bin/java
+fi
+
+
+IP=`ip addr | grep inet | grep -v "::" | grep -v "127.0.0.1" | awk {'print $2'} | cut -d"/" -f 1`
+if [ "$IP" == "" ]
+then
+        echo "Error cannot determine IP. Exiting ..." > /var/log/consul.log
+        exit
+fi
+
+
+
+added=`curl http://localhost:8500/v1/kv/cassandra/seed?raw | grep "1P" | wc -l`
+if [ $added -eq 0 ]
+then
+        tmp=`curl http://localhost:8500/v1/kv/cassandra/seed?raw`
+        if [ "$tmp" == "" ]
+        then
+                curl -X PUT -d "$IP" http://localhost:8500/v1/kv/cassandra/seed?raw
+        else
+                curl -X PUT -d "$tmp,$IP" http://localhost:8500/v1/kv/cassandra/seed?raw
+        fi
+fi
+
+
+
+
+# Check with consul
+data=""
+while [ "$data" == "" ]
+do
+	data=`curl http://localhost:8500/v1/kv/cassandra/node_count?raw`
+	#compare
+	# check if seed is empty
+	# check if seed has one entry
+	# check the number of entries
+	if [ "$data" != "" ]
+	then
+		SEED=`curl http://localhost:8500/v1/kv/cassandra/seed?raw`
+		if [ "$SEED" != "" ]
+		then
+			nodes_reg=`echo $SEED | grep -o "," | wc -l`
+			nodes_reg=`expr $nodes_reg + 1`
+			if [ $data -ne $nodes_reg ]
+			then
+				data=""
+			fi
+		else
+			data=""
+		fi
+	fi
+	sleep 3
+done
+
+
+
+# Configure 
+sed -i "s/<cluster>/$CLUSTERNAME/g"  /opt/cassandra/conf/cassandra.yaml
+sed -i "s/<seed>/$SEED/g"  /opt/cassandra/conf/cassandra.yaml
+sed -i "s/<ip>/$IP/g" /opt/cassandra/conf/cassandra.yaml
+
+sed -i "s/<MEM>/$MEM/g"  /opt/cassandra/conf/cassandra-env.sh
+sed -i "s/<HEAP>/$HEAP/g"  /opt/cassandra/conf/cassandra-env.sh
+
+
+source /etc/profile
+systemctl enable cassandra
+service cassandra restart
+
